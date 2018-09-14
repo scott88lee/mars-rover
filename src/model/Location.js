64862819489f04728d9c0d5771c7934bd012a0ef
songguoqiang <songguoq@gmail.com>
2018-09-14 20:54:35 +0800

    support move forward/backward

--- src/model/Location.js
+++ src/model/Location.js
@@ -1,6 +1,33 @@
+const { NORTH, SOUTH, EAST, WEST } = require("./Direction");
+
 module.exports = class Location {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  moveForward(direction) {
+    switch (direction) {
+      case NORTH:
+        return new Location(this.x, this.y + 1);
+      case SOUTH:
+        return new Location(this.x, this.y - 1);
+      case EAST:
+        return new Location(this.x + 1, this.y);
+      case WEST:
+        return new Location(this.x - 1, this.y);
+    }
+  }
+  moveBackward(direction) {
+    switch (direction) {
+      case NORTH:
+        return new Location(this.x, this.y - 1);
+      case SOUTH:
+        return new Location(this.x, this.y + 1);
+      case EAST:
+        return new Location(this.x - 1, this.y);
+      case WEST:
+        return new Location(this.x + 1, this.y);
+    }
   }
 };
--- src/model/Location.test.js
+++ src/model/Location.test.js
@@ -1,4 +1,5 @@
 const Location = require("./Location");
+const { NORTH, SOUTH, EAST, WEST } = require("./Direction");
 
 describe("Tests for Location", () => {
   test("create location with x and y coordinates", () => {
@@ -7,5 +8,60 @@ describe("Tests for Location", () => {
     let location = new Location(x, y);
     expect(location.x).toEqual(x);
     expect(location.y).toEqual(y);
+  });
+  test("move forward when facing north", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(0, 1);
+    let currentDirection = NORTH;
+    expect(currentLocation.moveForward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move backward when facing north", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(0, -1);
+    let currentDirection = NORTH;
+    expect(currentLocation.moveBackward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move forward when facing south", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(0, -1);
+    let currentDirection = SOUTH;
+    expect(currentLocation.moveForward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move backward when facing south", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(0, 1);
+    let currentDirection = SOUTH;
+    expect(currentLocation.moveBackward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move forward when facing east", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(1, 0);
+    let currentDirection = EAST;
+    expect(currentLocation.moveForward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move backward when facing east", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(-1, 0);
+    let currentDirection = EAST;
+    expect(currentLocation.moveBackward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move forward when facing west", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(-1, 0);
+    let currentDirection = WEST;
+    expect(currentLocation.moveForward(currentDirection)).toEqual(newLocation);
+  });
+
+  test("move backward when facing west", () => {
+    let currentLocation = new Location(0, 0);
+    let newLocation = new Location(1, 0);
+    let currentDirection = WEST;
+    expect(currentLocation.moveBackward(currentDirection)).toEqual(newLocation);
   });
 });
