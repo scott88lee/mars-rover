99e2bc8e032e9c24927830fcd86e068da8f75a3e
songguoqiang <songguoq@gmail.com>
2018-09-14 17:38:07 +0800

    add model class for location

---
+++ src/model/Location.test.js
@@ -0,0 +1,11 @@
+const Location = require("./Location")
+
+describe("Tests for Location", () => {
+  test("create location with x and y coordinates", () => {
+    let x = 0;
+    let y = 0;
+    let location = new Location(x, y);
+    expect(location.x).toEqual(x);
+    expect(location.y).toEqual(y);
+  });
+});
